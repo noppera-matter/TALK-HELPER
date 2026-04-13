2f93db4d1fd95930f406be504741f4b4fc16f353
noppera-matter <hyejungattractive@gmail.com>
2026-04-13 17:54:39 +0900

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2050015879" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2448015098" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
